77318d87e4cbb3eacdd93cb77fd878b5370759cb
maiquelleonel <skywishmtfk@gmail.com>
2010-01-30 01:08:55 -0200

    correção no arquivo de conexao ao DB

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysqli://root:local@localhost/liapires';
+$db_url = 'mysqli://root:local@localhost/drupal';
 $db_prefix = '';
 
 /**
